136345adcb88912f6f5a58ad8d6a267fd603a16f
Felipe Pagani <feh.pagani@gmail.com>
2021-03-26 16:17:52 -0300

    Project created

---
+++ src/test/java/com/treinamento/coursespringbot/CourseSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.coursespringbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
