2d9303550472f55dad6e7c5a56849984247b48c6
Felipe Pagani <feh.pagani@gmail.com>
2021-03-29 14:58:44 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/treinamento/coursespringbot/entities/User.java
+++ src/main/java/com/treinamento/coursespringbot/entities/User.java
@@ -1,30 +1,40 @@
 package com.treinamento.coursespringbot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 //dar prioridade a especificação
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable{
-	//Quando o objeto e transformado em cadeia de bytes
+@Table(name = "tb_user")
+public class User implements Serializable {
+	// Quando o objeto e transformado em cadeia de bytes
 	private static final long serialVersionUID = 1L;
 	@Id
-	//definição de estrategia de auto_increment
+	// definição de estrategia de auto_increment
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	// Caso for uma coleção -> ArrayList: é somente o get!
+	//Ele está mapeado pelo atributo client
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
+
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -33,38 +43,51 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.password = password;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,6 +96,7 @@ public class User implements Serializable{
 		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -94,14 +118,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
