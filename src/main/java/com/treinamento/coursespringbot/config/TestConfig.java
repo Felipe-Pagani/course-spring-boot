2032869881b3740768fdbf9af72a595cefa48cf4
Felipe Pagani <feh.pagani@gmail.com>
2021-03-31 16:48:58 -0300

    Payment, one-to-one association

--- src/main/java/com/treinamento/coursespringbot/config/TestConfig.java
+++ src/main/java/com/treinamento/coursespringbot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.treinamento.coursespringbot.entities.Category;
 import com.treinamento.coursespringbot.entities.Order;
 import com.treinamento.coursespringbot.entities.OrderItem;
+import com.treinamento.coursespringbot.entities.Payment;
 import com.treinamento.coursespringbot.entities.Product;
 import com.treinamento.coursespringbot.entities.User;
 import com.treinamento.coursespringbot.entities.enums.OrderStatus;
@@ -84,6 +85,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2020-11-20T12:20:00Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 }
