62be9fdaa10d2c17566b9549470f0f75b66137e2
Felipe Pagani <feh.pagani@gmail.com>
2021-04-01 17:28:09 -0300

    Exception handling - delete

--- src/main/java/com/treinamento/coursespringbot/services/UserService.java
+++ src/main/java/com/treinamento/coursespringbot/services/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.treinamento.coursespringbot.entities.User;
+import com.treinamento.coursespringbot.exceptions.DataBaseException;
 import com.treinamento.coursespringbot.exceptions.ResourceNotFoundException;
 import com.treinamento.coursespringbot.repositories.UserRepository;
 
@@ -37,7 +40,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException	e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}		
 		
 	}
 	public User update(Long id, User obj) {
